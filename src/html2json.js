672c290840fca39d2dfa2563134bde7d3e7f80ba
Steven Daniel <dstevie9292@gmail.com>
2021-08-26 23:29:26 +0100

    improvements

--- src/html2json.js
+++ src/html2json.js
@@ -163,7 +163,6 @@
     if (json.attr) {
       attr = Object.keys(json.attr).map(function(key) {
         var value = json.attr[key];
-        console.log(key);
         if(isObject(value) && !key.startsWith('data')){
           var styleString = `${key}="`;
           Object.keys(value).forEach(val => {
